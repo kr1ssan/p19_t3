a9e0b39951b803bd8e56baf31b96cd13f5e527d4
Redmi <Redmi@REDMIBOOK-IGOR>
2023-02-16 10:53:55 +0300

    Добавьте файлы проекта.

---
+++ p19_t3/HashTable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p19_t3
+{
+    class Element
+    {
+        public string Key { get; private set; }
+        public string Value { get; private set; }
+
+        public Element(string Key, string Value)
+        {
+            if (string.IsNullOrEmpty(Key))
+            {
+                throw new ArgumentNullException(nameof(Key));
+            }
+
+            if (string.IsNullOrEmpty(Value))
+            {
+                throw new ArgumentNullException(nameof(Value));
+            }
+
+            this.Key = Key;
+            this.Value = Value;
+        }
+        
+        
+
+        public override string ToString()
+        {
+            return Key;
+        }
+    }
+
+    class HashTable
+    {
+        int size;
+        public HashTable(int size)
+        {
+            this.size = size;
+        }
+        int Hash(int key)
+        {
+            return key % size;
+        }
+    }
+}
---
+++ p19_t3/Program.cs
@@ -0,0 +1,12 @@
+﻿using p19_t3;
+
+namespace p19_t3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
